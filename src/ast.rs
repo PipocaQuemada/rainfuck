dec4ad890606a61aeb34654ddcbac2c967e05995
Elliot Stern <eliyahu.ben.miney@gmail.com>
2018-09-23 14:02:14 -0400

    Ran rustfmt

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,7 @@
 //extern crate nom;
-use std::str::Chars;
-use std::io::Read;
 use std::io;
-
+use std::io::Read;
+use std::str::Chars;
 
 #[derive(Debug)]
 pub enum AST {
@@ -12,7 +11,7 @@ pub enum AST {
     Right,
     Read,
     Write,
-    Loop(Vec<AST>)
+    Loop(Vec<AST>),
 }
 
 pub fn parse(code: &mut Chars) -> Option<Vec<AST>> {
@@ -24,22 +23,26 @@ fn parseLoop(code: &mut Chars, isLoop: bool) -> Option<Vec<AST>> {
     loop {
         let c = match code.next() {
             Some(c) => c,
-            None => break
+            None => break,
         };
         match c {
-            '+' => acc.push(AST::Incr), 
+            '+' => acc.push(AST::Incr),
             '-' => acc.push(AST::Decr),
-            '>' => acc.push(AST::Right), 
+            '>' => acc.push(AST::Right),
             '<' => acc.push(AST::Left),
             ',' => acc.push(AST::Read),
             '.' => acc.push(AST::Write),
             '[' => match parseLoop(code, true) {
-                    Some(l) => acc.push(AST::Loop(l)),
-                    None => return None
-                   }
-            ']' => if isLoop { return(Some(acc)) } else { return None },
-            ' ' | '\n'  => (),
-            _ => return None
+                Some(l) => acc.push(AST::Loop(l)),
+                None => return None,
+            },
+            ']' => if isLoop {
+                return (Some(acc));
+            } else {
+                return None;
+            },
+            ' ' | '\n' => (),
+            _ => return None,
         }
     }
     if isLoop {
@@ -57,26 +60,34 @@ pub fn executeWithVec(code: &Vec<AST>, data: &mut Vec<u8>, mut pointer: usize) -
         match instruction {
             AST::Incr => data[pointer] += 1,
             AST::Decr => data[pointer] -= 1,
-            AST::Left => if pointer > 0 { pointer -= 1 } else { return None },
-            AST::Right => if pointer < data.len() - 1 { pointer += 1 } else { return None } ,
+            AST::Left => if pointer > 0 {
+                pointer -= 1
+            } else {
+                return None;
+            },
+            AST::Right => if pointer < data.len() - 1 {
+                pointer += 1
+            } else {
+                return None;
+            },
             AST::Read => match bytes.next() {
-                      Some(Ok(c)) => data[pointer] = c,
-                      None | Some(Err(_)) => return None
-                    },
+                Some(Ok(c)) => data[pointer] = c,
+                None | Some(Err(_)) => return None,
+            },
             AST::Write => print!("{}", char::from(data[pointer])),
-            AST::Loop(loopCode) =>
-                if data[pointer] != 0 {
-                        loop {
-                           //println!("starting loop iteration.");
-                           match executeWithVec(&loopCode, data, pointer) {
-                                Some(p) => pointer = p, // advance the pointer to wherever it was after the loop finished
-                                None => return None
-                           }
-                           //println!("finished loop iteration. pointer: {:?}, byte at pointer: {:?}", pointer, data[pointer]);
-                           if data[pointer] == 0 { break }
-                        }                         
-                   }
-                
+            AST::Loop(loopCode) => if data[pointer] != 0 {
+                loop {
+                    //println!("starting loop iteration.");
+                    match executeWithVec(&loopCode, data, pointer) {
+                        Some(p) => pointer = p, // advance the pointer to wherever it was after the loop finished
+                        None => return None,
+                    }
+                    //println!("finished loop iteration. pointer: {:?}, byte at pointer: {:?}", pointer, data[pointer]);
+                    if data[pointer] == 0 {
+                        break;
+                    }
+                }
+            },
         }
     }
     Some(pointer)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use std::env;
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::fs::File;
-use std::env;
 
 mod ast;
 
@@ -11,25 +11,23 @@ fn main() {
     let filename = &args[1];
     let contents = readSource(filename).unwrap();
 
-    let parsed = ast::parse(&mut contents.chars()); 
-
+    let parsed = ast::parse(&mut contents.chars());
 
     println!("{:?}", contents);
     println!("{:?}", parsed);
 
-    let code = parsed.unwrap_or(Vec::new()); 
+    let code = parsed.unwrap_or(Vec::new());
 
     println!("{:?}", code);
     ast::executeWithVec(&code, &mut vec![0; 30000], 0);
 }
 
-
 fn readSource(filename: &str) -> std::io::Result<String> {
-  let file = File::open(filename)?;
-  let mut reader = BufReader::new(file);
-  let mut buffer = String::new();
- 
-  reader.read_to_string(&mut buffer);
+    let file = File::open(filename)?;
+    let mut reader = BufReader::new(file);
+    let mut buffer = String::new();
 
-  Ok(buffer)
+    reader.read_to_string(&mut buffer);
+
+    Ok(buffer)
 }
